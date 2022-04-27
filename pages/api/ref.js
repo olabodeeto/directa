d71a56cba5df0f58edb4d1011a1b2f1656e6a513
etrustfund <technology@ayeorganization.com>
2022-04-27 13:12:14 +0100

    try again

--- pages/api/ref.js
+++ pages/api/ref.js
@@ -3,21 +3,23 @@ import User from "../../models/userModel";
 import Ref from "../../models/refModel";
 import protectAPI from "../../middlewares/protectAPI";
 
-let secret = "sec_YToy9KMlSJV7w97ZsCXc";
+let secret = "sec_YToy9KMlSJV7w97ZsCXc"; //test mode sk
 
 async function handler(req, res) {
   const webhook = req.body;
 
   switch (webhook.event) {
     case "direct_debit.payment_successful":
-      // do something with webhook.data.account;
+      // do something with webhook data;
       try {
         const ref = new Ref({
           amount: req.body.data.object.amount,
           reference: req.body.data.object.reference,
         });
+
         const result = await ref.save(); //save the webhook returned data
         if (!result) return;
+
         //Get the pending transaction details using the webhook reference
         const trans = await Trans.findOne({
           reference: req.body.data.object.reference,
@@ -25,6 +27,7 @@ async function handler(req, res) {
 
         if (!trans) return;
         const transMemberID = trans.memberID;
+
         // find the member using the pending transaction member's id
         const user = await User.findOne({ _id: transMemberID });
         if (!user) return;
@@ -36,7 +39,7 @@ async function handler(req, res) {
 
         const memberAccountUpdate = await User.findOneAndUpdate(
           { _id: transMemberID },
-          { balance: newBalance, planActive: true },
+          { balance: newBalance, planActive: true }, //update the balance
           { new: true },
           (error, result) => {
             if (result) {
@@ -52,6 +55,24 @@ async function handler(req, res) {
           });
 
         if (memberAccountUpdate) {
+          const memberTransactionUpdate = await Trans.findOneAndUpdate(
+            { reference: req.body.data.object.reference },
+            { status: "true" },
+            { new: true },
+            (error, result) => {
+              if (result) {
+                return result;
+              } else {
+                console.log(error);
+              }
+            }
+          )
+            .clone()
+            .catch(function (err) {
+              console.log(err);
+            });
+
+          if (!memberTransactionUpdate) return;
           res.status(200).send(result);
         }
       } catch (error) {
@@ -59,51 +80,6 @@ async function handler(req, res) {
       }
       break;
   }
-
-  //   if (webhook.event === "direct_debit.payment_successful") {
-  //     const amount = parseInt(req.body.data.object.amount) / 100;
-  //     const ref = new Ref({
-  //       amount: req.body.data.object.amount,
-  //       reference: req.body.data.object.reference,
-  //     });
-
-  //     try {
-  //       const result = await ref.save(); //save user info
-  //       if (result) {
-  //         const trans = await Trans.findOne({
-  //           reference: req.body.data.object.reference,
-  //         });
-
-  //         const transMemberID = trans.memberID;
-  //         const user = await User.findOne({ _id: transMemberID });
-  //         if (!user) return;
-  //         const currentUserBalance = user.balance;
-  //         const newBalance = amount + currentUserBalance;
-
-  //         const memberAccountUpdate = await User.findOneAndUpdate(
-  //           { _id: transMemberID },
-  //           { balance: newBalance, planActive: true },
-  //           { new: true },
-  //           (error, result) => {
-  //             if (result) {
-  //               return result;
-  //             } else {
-  //               console.log(error);
-  //             }
-  //           }
-  //         )
-  //           .clone()
-  //           .catch(function (err) {
-  //             console.log(err);
-  //           });
-
-  //         res.status(200).send(result);
-  //       }
-  //     } catch (error) {
-  //       res.status(500).send({ message: "Error" });
-  //     }
-  //   }
-  //   res.status(200).json("success");
 }
 
 export default protectAPI(handler);
