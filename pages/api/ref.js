32b1bd339520a4125ec50e7412e07c42dac26816
etrustfund <technology@ayeorganization.com>
2022-04-27 12:37:34 +0100

    try again

--- pages/api/ref.js
+++ pages/api/ref.js
@@ -16,35 +16,42 @@ async function handler(req, res) {
           amount: req.body.data.object.amount,
           reference: req.body.data.object.reference,
         });
-        const result = await ref.save(); //save user info
-        if (result) {
-          const trans = await Trans.findOne({
-            reference: req.body.data.object.reference,
+        const result = await ref.save(); //save the webhook returned data
+        if (!result) return;
+        //Get the pending transaction details using the webhook reference
+        const trans = await Trans.findOne({
+          reference: req.body.data.object.reference,
+        });
+
+        if (!trans) return;
+        const transMemberID = trans.memberID;
+        // find the member using the pending transaction member's id
+        const user = await User.findOne({ _id: transMemberID });
+        if (!user) return;
+
+        //Calcutale the new member balance
+        const currentUserBalance = user.balance;
+        const amount = parseInt(user.savingsAmount);
+        const newBalance = amount + currentUserBalance;
+
+        const memberAccountUpdate = await User.findOneAndUpdate(
+          { _id: transMemberID },
+          { balance: newBalance, planActive: true },
+          { new: true },
+          (error, result) => {
+            if (result) {
+              return result;
+            } else {
+              console.log(error);
+            }
+          }
+        )
+          .clone()
+          .catch(function (err) {
+            console.log(err);
           });
 
-          const transMemberID = trans.memberID;
-          const user = await User.findOne({ _id: transMemberID });
-          if (!user) return;
-          const currentUserBalance = user.balance;
-          const newBalance = amount + currentUserBalance;
-
-          const memberAccountUpdate = await User.findOneAndUpdate(
-            { _id: transMemberID },
-            { balance: newBalance, planActive: true },
-            { new: true },
-            (error, result) => {
-              if (result) {
-                return result;
-              } else {
-                console.log(error);
-              }
-            }
-          )
-            .clone()
-            .catch(function (err) {
-              console.log(err);
-            });
-
+        if (memberAccountUpdate) {
           res.status(200).send(result);
         }
       } catch (error) {
