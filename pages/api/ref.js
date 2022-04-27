bd8c703ee81518c70dd10e76a5c21dcf9946c73e
etrustfund <technology@ayeorganization.com>
2022-04-27 10:44:22 +0100

    try again

--- pages/api/ref.js
+++ pages/api/ref.js
@@ -1,5 +1,6 @@
 import Trans from "../../models/transactionModel";
 import User from "../../models/userModel";
+import Ref from "../../models/refModel";
 import protectAPI from "../../middlewares/protectAPI";
 
 let secret = "sec_YToy9KMlSJV7w97ZsCXc";
@@ -16,58 +17,72 @@ async function handler(req, res) {
     });
 
     if (trans) {
-      const transMemberID = trans.memberID;
-      // find user using the memberid from pending transaction
-      const user = await User.findOne({ _id: transMemberID });
-      if (!user) return;
-      const currentUserBalance = user.balance;
-      const newBalance = amount + currentUserBalance;
-
-      const memberAccountUpdate = await User.findOneAndUpdate(
-        { _id: transMemberID },
-        { balance: newBalance, planActive: true },
-        { new: true },
-        (error, result) => {
-          if (result) {
-            return result;
-          } else {
-            console.log(error);
-          }
-        }
-      )
-        .clone()
-        .catch(function (err) {
-          console.log(err);
+      try {
+        const ref = new Ref({
+          amount: req.body.data.object.amount,
+          reference: req.body.data.object.reference,
         });
-
-      if (memberAccountUpdate._id) {
-        const memberTransactionUpdate = await Trans.findOneAndUpdate(
-          { reference: hookreference },
-          { status: "true" },
-          { new: true },
-          (error, result) => {
-            if (result) {
-              return result;
-            } else {
-              console.log(error);
-            }
-          }
-        )
-          .clone()
-          .catch(function (err) {
-            console.log(err);
-          });
-
-        if (memberTransactionUpdate._id) {
-          console.log(memberTransactionUpdate);
-          res.status(200).json("success");
+        const result = await ref.save(); //save user info
+        if (result) {
+          console.log(result);
+          res.status(200).send(result);
         }
+      } catch (error) {
+        res.status(500).send({ message: "Error" });
       }
     }
+
+    // if (trans) {
+    //   const transMemberID = trans.memberID;
+    //   // find user using the memberid from pending transaction
+    //   const user = await User.findOne({ _id: transMemberID });
+    //   if (!user) return;
+    //   const currentUserBalance = user.balance;
+    //   const newBalance = amount + currentUserBalance;
+
+    //   const memberAccountUpdate = await User.findOneAndUpdate(
+    //     { _id: transMemberID },
+    //     { balance: newBalance, planActive: true },
+    //     { new: true },
+    //     (error, result) => {
+    //       if (result) {
+    //         return result;
+    //       } else {
+    //         console.log(error);
+    //       }
+    //     }
+    //   )
+    //     .clone()
+    //     .catch(function (err) {
+    //       console.log(err);
+    //     });
+
+    //   if (memberAccountUpdate._id) {
+    //     const memberTransactionUpdate = await Trans.findOneAndUpdate(
+    //       { reference: hookreference },
+    //       { status: "true" },
+    //       { new: true },
+    //       (error, result) => {
+    //         if (result) {
+    //           return result;
+    //         } else {
+    //           console.log(error);
+    //         }
+    //       }
+    //     )
+    //       .clone()
+    //       .catch(function (err) {
+    //         console.log(err);
+    //       });
+
+    //     if (memberTransactionUpdate._id) {
+    //       console.log(memberTransactionUpdate);
+    //       res.status(200).json("success");
+    //     }
+    //   }
+    // }
   }
 }
-
-export default protectAPI(handler);
 
 // import Ref from "../../models/refModel";
 // import protectAPI from "../../middlewares/protectAPI";
@@ -96,3 +111,5 @@ export default protectAPI(handler);
 // }
 
 // export default protectAPI(handler);
+
+export default protectAPI(handler);
