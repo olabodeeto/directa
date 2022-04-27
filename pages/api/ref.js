f5d52db8c9939fbea0b6fad7a2848956302408aa
etrustfund <technology@ayeorganization.com>
2022-04-27 11:25:59 +0100

    try again

--- pages/api/ref.js
+++ pages/api/ref.js
@@ -1,88 +1,5 @@
-// import Trans from "../../models/transactionModel";
-// import User from "../../models/userModel";
-// import Ref from "../../models/refModel";
-// import protectAPI from "../../middlewares/protectAPI";
-
-// let secret = "sec_YToy9KMlSJV7w97ZsCXc";
-
-// async function handler(req, res) {
-//   const webhook = req.body;
-
-//   if (webhook.event === "direct_debit.payment_successful") {
-//     const hookreference = req.body.data.object.reference;
-//     const amount = parseInt(req.body.data.object.amount);
-
-//     const trans = await Trans.findOne({
-//       reference: req.body.data.object.reference,
-//     });
-
-//     if (trans._id) {
-//       try {
-//         const ref = new Ref({
-//           amount: req.body.data.object.amount,
-//           reference: req.body.data.object.reference,
-//         });
-//         const result = await ref.save(); //save user info
-//         if (result) {
-//           res.status(200).send(result);
-//         }
-//       } catch (error) {
-//         res.status(500).send({ message: "Error" });
-//       }
-//     }
-
-// if (trans) {
-//   const transMemberID = trans.memberID;
-//   // find user using the memberid from pending transaction
-//   const user = await User.findOne({ _id: transMemberID });
-//   if (!user) return;
-//   const currentUserBalance = user.balance;
-//   const newBalance = amount + currentUserBalance;
-
-//   const memberAccountUpdate = await User.findOneAndUpdate(
-//     { _id: transMemberID },
-//     { balance: newBalance, planActive: true },
-//     { new: true },
-//     (error, result) => {
-//       if (result) {
-//         return result;
-//       } else {
-//         console.log(error);
-//       }
-//     }
-//   )
-//     .clone()
-//     .catch(function (err) {
-//       console.log(err);
-//     });
-
-//   if (memberAccountUpdate._id) {
-//     const memberTransactionUpdate = await Trans.findOneAndUpdate(
-//       { reference: hookreference },
-//       { status: "true" },
-//       { new: true },
-//       (error, result) => {
-//         if (result) {
-//           return result;
-//         } else {
-//           console.log(error);
-//         }
-//       }
-//     )
-//       .clone()
-//       .catch(function (err) {
-//         console.log(err);
-//       });
-
-//     if (memberTransactionUpdate._id) {
-//       console.log(memberTransactionUpdate);
-//       res.status(200).json("success");
-//     }
-//   }
-// }
-//   }
-// }
-
+import Trans from "../../models/transactionModel";
+import User from "../../models/userModel";
 import Ref from "../../models/refModel";
 import protectAPI from "../../middlewares/protectAPI";
 
@@ -91,6 +8,7 @@ let secret = "sec_YToy9KMlSJV7w97ZsCXc";
 async function handler(req, res) {
   const webhook = req.body;
   if (webhook.event === "direct_debit.payment_successful") {
+    const amount = parseInt(req.body.data.object.amount) / 100;
     const ref = new Ref({
       amount: req.body.data.object.amount,
       reference: req.body.data.object.reference,
@@ -99,7 +17,33 @@ async function handler(req, res) {
     try {
       const result = await ref.save(); //save user info
       if (result) {
-        console.log(result);
+        const trans = await Trans.findOne({
+          reference: req.body.data.object.reference,
+        });
+
+        const transMemberID = trans.memberID;
+        const user = await User.findOne({ _id: transMemberID });
+        if (!user) return;
+        const currentUserBalance = user.balance;
+        const newBalance = amount + currentUserBalance;
+
+        const memberAccountUpdate = await User.findOneAndUpdate(
+          { _id: transMemberID },
+          { balance: newBalance, planActive: true },
+          { new: true },
+          (error, result) => {
+            if (result) {
+              return result;
+            } else {
+              console.log(error);
+            }
+          }
+        )
+          .clone()
+          .catch(function (err) {
+            console.log(err);
+          });
+
         res.status(200).send(result);
       }
     } catch (error) {
